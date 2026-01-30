0d0bc61c5c9707c99f1bb4175b3d8aa6407367e2
Pc2 <Pc2@Villapc2>
2026-01-29 20:26:08 -0600

    CodigoPrincipal

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Ingresa opcion: ");
+        int a = sc.nextInt();
+        System.out.println("Ingresa el segundo numero: ");
+        int b = sc.nextInt();
+        
+       // Suma suma = new suma();
+        
+        //System.out.println("La suma es "+suma.calculadora(a,b));
+        
+    }
+    
+}
